a7357ee939d473c6a164c58d3c0d890cffbdb4fa
trukhachev495 <sergey.trainings@gmail.com>
2023-05-11 16:49:28 +0400

    completed one task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,16 +1,16 @@
-"use strict";
+'use strict'
 const solveEquation = (a, b, c) => {
- let start = b**2-4*a*c
- if (start < 0) {
-  return [];
- }
- if(start===0) {
-   return [-b/(2*a)]
+  let start = b ** 2 - 4 * a * c
+  if (start < 0) {
+    return []
   }
- if(start > 0) {
-  let first = (-b + Math.sqrt(start) )/(2*a)
-  let second = (-b - Math.sqrt(start) )/(2*a)
-   return [first,second]
+  if (start === 0) {
+    return [-b / (2 * a)]
+  }
+  if (start > 0) {
+    let first = (-b + Math.sqrt(start)) / (2 * a)
+    let second = (-b - Math.sqrt(start)) / (2 * a)
+    return [first, second]
   }
 }
 
