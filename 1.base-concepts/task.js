1b07e861640acefc60115e9bb0572018bbbccd16
trukhachev495 <sergey.trainings@gmail.com>
2023-05-11 16:33:16 +0400

    completed 1 task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,19 @@
-"use strict"
-function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+"use strict";
+const solveEquation = (a, b, c) => {
+ let start = b**2-4*a*c
+ if (start < 0) {
+  return [];
+ }
+ if(start===0) {
+   return [-b/(2*a)]
+  }
+ if(start > 0) {
+  let first = (-b + Math.sqrt(start) )/(2*a)
+  let second = (-b - Math.sqrt(start) )/(2*a)
+   return [first,second]
+  }
 }
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+/*function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
-}
+}*/
