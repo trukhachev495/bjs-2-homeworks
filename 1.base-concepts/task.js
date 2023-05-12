5e850b870e2dd3818dfec7d27afef2d84a441847
trukhachev495 <sergey.trainings@gmail.com>
2023-05-12 19:25:39 +0400

    completed two task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -14,6 +14,28 @@ const solveEquation = (a, b, c) => {
   }
 }
 
-/*function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
-}*/
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  parseFloat(percent,contribution,amount,countMonths)
+ 
+  if (isNaN(percent)){
+    return `Параметр ${"'Процентая ставка'"} содержит неправильное значение ${percent}`
+  }
+  if (isNaN(contribution)){
+    return `Параметр ${"'Сумма первоначального взноса'"} содержит неправильное значение ${contribution}`
+  }
+  if (isNaN(amount)){
+    return `Параметр ${"'Cумма кредита'"} содержит неправильное значение ${amount}`
+  }
+  if (isNaN(countMonths)){
+    return `Параметр ${"'Срок(в месяцaх)'"} содержит неправильное значение ${countMonths}`
+  }
+
+  let amount1 = amount-contribution
+  let percent1 = percent/100/12
+  let monthlyPayment = amount1 * (percent1+(percent1 / (((1+percent1)**countMonths)-1))) /*Ежемесячный платеж*/
+  let totalPayment = monthlyPayment * countMonths
+ 
+  return totalPayment.toFixed(2)
+}
+
+
