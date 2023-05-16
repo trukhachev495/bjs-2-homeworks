436442725858c4155330ec34c53f37875624fc7a
trukhachev495 <sergey.trainings@gmail.com>
2023-05-16 15:32:15 +0400

    did the first job

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,4 +1,24 @@
 function getArrayParams(...arr) {
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i]; // обновляем минимальное значение
+    }
+
+    if (arr[i] > max) {
+      max = arr[i]; // обновляем максимальное значение
+    }
+  }
+
+  const sumArray = arr.reduce(
+  (accumulator, currentValue) => accumulator + currentValue,
+  sum
+);
+let avg1 = sumArray/arr.length
+let avg2 = avg1.toFixed(2)
+let avg = Number(avg2)
 
   return { min: min, max: max, avg: avg };
 }
