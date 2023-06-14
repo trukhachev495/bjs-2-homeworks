4d794de077828dde2c2f024cadead0272f2aed50
trukhachev495 <sergey.trainings@gmail.com>
2023-06-14 23:35:38 +0400

    Решил задание

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,35 @@
-function Student(name, gender, age) {
-  
+function Student (name, gender, age) {
+  this.name = name
+  this.gender = gender
+  this.age = age
+  this.marks = []
+  this.subject = null
 }
-
-Student.prototype.setSubject = function (subjectName) {
-  
+Student.prototype.setSubject = function (subject) {
+  this.subject = subject
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if (!this.marks) {
+    return (this.marks = undefined)
+  }
+  this.marks.push(...marks)
 }
 
 Student.prototype.getAverage = function () {
-  
+  if (!this.marks || this.marks.length === 0) {
+    return 0
+  } else {
+    let sum = this.marks.reduce(
+      (accumulator, currentValue) => accumulator + currentValue,
+      0
+    )
+    return sum / this.marks.length
+  }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  this.exclude = reason
+  delete this.marks
+  delete this.subject
 }
